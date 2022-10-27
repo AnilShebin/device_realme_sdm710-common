2f2af0755f3127d9e062c3fa0cff48f338b5df08
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2022-10-27 22:05:35 +0530

    sdm710-common: Enable R8 optimizations for SystemUI & system_server
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: AnilShebin <anilshebin@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -244,6 +244,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     KharaMeParts
 
+# Enable whole-program R8 Java optimizations for SystemUI and system_server
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.realme_sdm710
