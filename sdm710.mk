f644f6a5251e6d3dd598faa9ee8f72fa79810475
mikeNG <mikeioannina@gmail.com>
2023-09-06 10:16:15 +0530

    sdm710-common: Build Lineage Health HAL
     - Lets enjoy Charging Control Feature now
    Signed-off-by: yograjfire18 <yograjfire18@gmail.com>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -323,6 +323,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.realme_sdm710 \
     lights.sdm710
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
